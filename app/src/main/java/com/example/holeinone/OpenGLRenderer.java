3f9d855433fb8ade0381494c62fdb6e24bebfe10
Eric Guillemot <contact@eric-guillemot.fr>
2019-03-23 10:03:41 +0100

    first

---
+++ app/src/main/java/com/example/holeinone/OpenGLRenderer.java
@@ -0,0 +1,36 @@
+package com.example.holeinone;
+
+import android.content.Context;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class OpenGLRenderer implements GLSurfaceView.Renderer {
+
+    private Context context;
+    private Square square;
+
+    public OpenGLRenderer(Context c) {
+        context = c;
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        square = new Square(context);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        GLES20.glClearColor(1f,0,0,1f);
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+        square.draw();
+    }
+}
---
+++ app/src/main/java/com/example/holeinone/Square.java
@@ -0,0 +1,75 @@
+package com.example.holeinone;
+
+import android.content.Context;
+import android.opengl.GLES20;
+
+import com.example.holeinone.tools.BufferUtils;
+import com.example.holeinone.tools.ShaderProgram;
+import com.example.holeinone.tools.ShaderUtils;
+
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+public class Square {
+    private FloatBuffer vertexBuffer;
+    private ShaderProgram shader;
+    private int vertexBufferId;
+    private int vertexCount;
+    private int vertexStride;
+
+    static final int COORDS_PER_VERTEX = 3;
+    static final int COLORS_PER_VERTEX = 4;
+    static final int SIZE_OF_FLOAT = 4;
+    static final float squareCoords[] = {
+            -0.5f, 0.5f, 0, 1f, 0, 0, 1f,
+            -0.5f, -0.5f, 0, 0, 1f, 0, 1f,
+            0.5f, -0.5f, 0, 0, 0, 1f, 1f,
+            -0.5f, 0.5f, 0, 1f, 0, 0, 1f,
+            0.5f, -0.5f, 0, 0, 0, 1f, 1f,
+            0.5f, 0.5f, 0, 0, 1f, 0, 1f
+    };
+
+    public Square(Context c) {
+        setupShader(c);
+        setupVertexBuffer();
+    }
+
+    private void setupShader(Context c) {
+        shader = new ShaderProgram(
+                ShaderUtils.readShaderFileFromRawResource(c, R.raw.simple_vertex_shader),
+                ShaderUtils.readShaderFileFromRawResource(c, R.raw.simple_fragment_shader)
+        );
+    }
+
+    private void setupVertexBuffer() {
+        vertexBuffer = BufferUtils.newFloatBuffer(squareCoords.length);
+        vertexBuffer.put(squareCoords);
+        vertexBuffer.position(0);
+
+        IntBuffer buffer = IntBuffer.allocate(1);
+        GLES20.glGenBuffers(1, buffer);
+        vertexBufferId = buffer.get(0);
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, vertexBufferId);
+        GLES20.glBufferData(GLES20.GL_ARRAY_BUFFER, squareCoords.length * 4, vertexBuffer, GLES20.GL_STATIC_DRAW);
+
+        vertexCount = squareCoords.length / (COORDS_PER_VERTEX + COLORS_PER_VERTEX);
+        vertexStride = (COORDS_PER_VERTEX + COLORS_PER_VERTEX) * 4;
+    }
+
+    public void draw() {
+        shader.begin();
+        shader.enableVertexAttribute("a_Position");
+//        shader.enableVertexAttribute("a_Color");
+
+        shader.setVertexAttribute("a_Position", COORDS_PER_VERTEX, GLES20.GL_FLOAT, false, vertexStride, 0);
+//        shader.setVertexAttribute("a_Color", COLORS_PER_VERTEX, GLES20.GL_FLOAT, false, vertexStride, COORDS_PER_VERTEX * SIZE_OF_FLOAT);
+
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, vertexBufferId);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
+
+        shader.disableVertexAttribute("a_Position");
+//        shader.disableVertexAttribute("a_Color");
+
+        shader.end();
+    }
+}
